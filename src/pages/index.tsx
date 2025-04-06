f16d0cdddface2489907ec0b18b76b0111bfc798
Rayyan2008 <crunchyjuniper83@gmail.com>
2025-04-06 15:21:53 +0530

    Initial commit for Virtual Campus

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <Button>Click me</Button>
+    </main>
+  );
+}
